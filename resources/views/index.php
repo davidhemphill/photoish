aef9e330c3091f8b16194178cf83ede85c0e8168
David Hemphill <davidlee.hemphill@gmail.com>
2019-02-01 12:44:04 -0600

    wip

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en" class="h-full">
     <head>
-        <title>photoish - tiny image caching & manipulation
+        <title>photoish - tiny image caching & manipulation</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
